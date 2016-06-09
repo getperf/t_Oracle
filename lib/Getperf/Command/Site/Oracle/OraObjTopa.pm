74de8bf7672550991b20f5dd844e5ba1103a0c89
Minoru Furusawa <frsw3nr@msn.com>
2016-06-10 07:06:58 +0900

    v2.12

--- lib/Getperf/Command/Site/Oracle/OraObjTopa.pm
+++ lib/Getperf/Command/Site/Oracle/OraObjTopa.pm
@@ -1,9 +1,8 @@
-package Getperf::Command::Site::Oracle::OraSqlTop;
+package Getperf::Command::Site::Oracle::OraObjTopa;
 use strict;
 use warnings;
 use Data::Dumper;
 use Time::Piece;
-use Path::Class;
 use base qw(Getperf::Container);
 use Getperf::Command::Master::Oracle;
 
@@ -12,11 +11,11 @@ sub new {bless{},+shift}
 my $PURGE_RRD_HOUR = 3 * 24;
 
 sub update_cacti_graph_item {
-	my ($self, $data_info, $sql_hash, $graph_templates_item_id, $data_template_data_id, $rra ) = @_;
+	my ($self, $data_info, $obj_key, $graph_templates_item_id, $data_template_data_id, $rra ) = @_;
 
 	my $update_item =
 		"UPDATE graph_templates_item " .
-		"SET text_format = '$sql_hash' " .
+		"SET text_format = '$obj_key' " .
 		"WHERE id = $graph_templates_item_id";
 	$data_info->cacti_db_dml($update_item);
 
@@ -27,14 +26,14 @@ sub update_cacti_graph_item {
 	$data_info->cacti_db_dml($update_rrd);
 }
 
-sub purge_sql_hash_rrd_data {
+sub purge_object_rank_rrd_data {
 	my ($self, $data_info, $instance) = @_;
 
 	my $storage_dir = $data_info->{absolute_storage_dir};
 	my $limit_time  = $data_info->start_time_sec->epoch - $PURGE_RRD_HOUR * 3600;
 
-	# purge storage/Oracle/{sid}/device/ora_sql_top__*.rrd
-	my $rrdfile_filter = "$storage_dir/Oracle/$instance/device/ora_sql_top__\*.rrd";
+	# purge storage/Oracle/{sid}/device/ora_obj_topa__*.rrd
+	my $rrdfile_filter = "$storage_dir/Oracle/$instance/device/ora_obj_topa__\*.rrd";
 	open (my $in, "ls $rrdfile_filter |") || die "can't find '$rrdfile_filter' : $!";
 	while (my $rrdfile = <$in>) {
 		chomp $rrdfile;
@@ -49,14 +48,15 @@ sub purge_sql_hash_rrd_data {
 sub parse {
     my ($self, $data_info) = @_;
 
-	my (%results, %sql_stats);
+	my (%results, %obj_stats);
 	my $step = 3600;
 	my $n_top = 40;
-	my @headers = qw/executions disk_reads buffer_gets rows_processed cpu_time elapsed_time/;
+	my @headers = qw/logical_reads lr_ratio physical_reads pr_ratio physical_writes rw_ratio/;
+
 	my %graph_headers = (
-		"cpu_time"    => "SQL CPU Time Ranking",
-		"buffer_gets" => "SQL Buffer Get Ranking",
-		"disk_reads"  => "SQL Disk Read Ranking",
+		"logical_reads"   => "Object Logical Read Ranking",
+		"physical_reads"  => "Object Physical Read Ranking",
+		"physical_writes" => "Object Physical Write Ranking",
 	);
 
 	$data_info->step($step);
@@ -66,7 +66,7 @@ sub parse {
 	if (!$sec) {
 		return;
 	}
-	$self->purge_sql_hash_rrd_data($data_info, $instance);
+	$self->purge_object_rank_rrd_data($data_info, $instance);
 	open( my $in, $data_info->input_file ) || die "@!";
 	while (my $line = <$in>) {
 		$line=~s/(\r|\n)*//g;			# trim return code
@@ -74,18 +74,20 @@ sub parse {
 			$sec = localtime(Time::Piece->strptime($1, '%y/%m/%d %H:%M:%S'))->epoch;
 			next;
 		}
-		my ($timestamp, $sql_hash, @values) = split(/\s*\|\s*/, $line);
+		my ($timestamp, $owner, $segment_name, $bytes, $buffer_type, $object_type, $tablespace_name, @values) = split(/\s*\|\s*/, $line);
 		next if (!defined($timestamp) || $timestamp eq 'TIME');
+		my $object_key = join('.', $object_type, $owner, $segment_name);
+		$object_key=~s/[\$\s\\]/_/g;
 		my $col = 0;
 		map {
 			my $header = $headers[$col];
-			$results{$sql_hash}{$sec}{$header} = $_;
-			$sql_stats{$sql_hash}{$header}    += $_;
+			$results{$object_key}{$sec}{$header} = $_;
+			$obj_stats{$object_key}{$header}    += $_;
 			$col ++;
 		} @values[0..5];
 	}
 	close($in);
-
+# print Dumper \%results;
 	my $query_items =
 		"SELECT  g.local_graph_id, gi.sequence, " .
 		"    gi.id graph_templates_item_id, gi.text_format, " .
@@ -101,13 +103,13 @@ sub parse {
 		"    AND g.title_cache = '__graph_title__' " .
 		"ORDER BY gi.sequence";
 
-	for my $sort_key(qw/cpu_time buffer_gets disk_reads/) {
-		my @sql_ranks = sort { $sql_stats{$b}{$sort_key} <=> $sql_stats{$a}{$sort_key} } keys %sql_stats;
+	for my $sort_key(qw/logical_reads physical_reads physical_writes/) {
+		my @obj_ranks = sort { $obj_stats{$b}{$sort_key} <=> $obj_stats{$a}{$sort_key} } keys %obj_stats;
 		my $rank = 1;
-		for my $sql_hash(@sql_ranks) {
-			$data_info->regist_device($instance, 'Oracle', 'ora_sql_top', $sql_hash, undef, \@headers);
-			my $output = "Oracle/${instance}/device/ora_sql_top__${sql_hash}.txt";
-			$data_info->pivot_report($output, $results{$sql_hash}, \@headers);
+		for my $obj_key(@obj_ranks) {
+			$data_info->regist_device($instance, 'Oracle', 'ora_obj_topa', $obj_key, undef, \@headers);
+			my $output = "Oracle/${instance}/device/ora_obj_topa__${obj_key}.txt";
+			$data_info->pivot_report($output, $results{$obj_key}, \@headers);
 			$rank ++;
 			last if ($n_top < $rank);
 		}
@@ -121,13 +123,13 @@ sub parse {
 				for my $row(@$rows) {
 					my $graph_templates_item_id = $row->[2] || 0;
 					my $data_template_data_id   = $row->[4] || 0;
-					my $sql_hash = shift(@sql_ranks);
-					if ($sql_hash && $graph_templates_item_id && $data_template_data_id) {
-						my $rra = "<path_rra>/Oracle/${instance}/device/ora_sql_top__${sql_hash}.rrd";
-						$self->update_cacti_graph_item($data_info, $sql_hash, $graph_templates_item_id,
+					my $obj_key = shift(@obj_ranks);
+					if ($obj_key && $graph_templates_item_id && $data_template_data_id) {
+						my $rra = "<path_rra>/Oracle/${instance}/device/ora_obj_topa__${obj_key}.rrd";
+						$self->update_cacti_graph_item($data_info, $obj_key, $graph_templates_item_id,
 							                           $data_template_data_id, $rra);
 					} else {
-						my $rra = "<path_rra>/Oracle/${instance}/device/ora_sql_top__dummy.rrd";
+						my $rra = "<path_rra>/Oracle/${instance}/device/ora_obj_topa__dummy.rrd";
 						$self->update_cacti_graph_item($data_info,, 'unkown', $graph_templates_item_id,
 							                           $data_template_data_id, $rra);
 					}
